57387894695dd04f7f3f1a265494778e47abebf9
hendraedugames <mikaazkakeisha@gmail.com>
2026-07-24 19:39:52 +0700

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = 'super-perkalian-v1';
+const FILES_TO_CACHE = [
+  './index.html',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png',
+  './fonts/Geostar.ttf',
+  './fonts/Orbitron.ttf'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      return cached || fetch(event.request).then((resp) => {
+        return caches.open(CACHE_NAME).then((cache) => {
+          if (event.request.method === 'GET' && resp.status === 200) {
+            cache.put(event.request, resp.clone());
+          }
+          return resp;
+        });
+      }).catch(() => cached);
+    })
+  );
+});
